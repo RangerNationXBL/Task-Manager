484a02676ab40bbd28b0367f9911083229e7158b
Chris <brown.christopher1978@gmail.com>
2023-11-28 13:00:43 -0800

    Cleaned up the variables inside of this program.

--- taskmanager/main.cpp
+++ taskmanager/main.cpp
@@ -10,15 +10,24 @@
 // https://twitter.com/fullyinvoled
 
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-using namespace std;
+#include <iostream> // Basic input and output
+#include <fstream> // File stream for opening, writing, and closing files
+#include <vector> // Data Structure
+using namespace std; // I was going to do this a different way, but decided to use it last minute.
+
+// The file will have several fields, maybe an addictional feature to eventually add it to a database.
+// - Task Name
+// - Task description
+// - Task priority
+// - Task Due Date
+// -- Task Index Number.
+// - Add times stamp to each entry
 
 class Task {
 public:
-    Task(const string& description, int priority, const string& dueDate)
-        : description(description), priority(priority), dueDate(dueDate), completed(false) {}
+    // Constructor
+    Task(const string& taskName, const string& description, int priority, const string& dueDate)
+        : taskName(taskName), description(description), priority(priority), dueDate(dueDate), completed(false) {}
 
 
     void markAsComplete() {
@@ -26,6 +35,7 @@ public:
     }
 
     void displayTask() const {
+        cout << "Task Name: " << taskName << "\n";
         cout << "Description: " << description << "\n";
         cout << "Priority: " << priority << "\n";
         cout << "Due Date: " << dueDate << "\n";
@@ -40,17 +50,29 @@ public:
         return description;
     }
 
+    const string& getDueDate() const{
+        return dueDate;
+    }
+
+    const string& getTaskName() const{
+        return taskName;
+    }
+
 private:
-    string description;
+    string taskName, description, dueDate;
     int priority;
-    string dueDate;
     bool completed;
 };
 
 void addTask(vector<Task>& taskList) {
-    string description;
+    string taskName, description, dueDate;
     int priority;
-    string dueDate;
+
+    // Add time stamp for entry date and time.
+
+    cout << "Enter the name of the task: ";
+    cin.ignore();
+    getline(cin, taskName);
 
     cout << "Please enter the details of the task: "; 
     cin.ignore();
@@ -62,7 +84,8 @@ void addTask(vector<Task>& taskList) {
     cout << "Enter a due date: ";
     cin >> dueDate;
 
-    Task newTask(description, priority, dueDate);
+    // An object is created to send to the vector
+    Task newTask(taskName, description, priority, dueDate);
     taskList.push_back(newTask);
 
     cout << "Task added successfully!" << endl << endl;
@@ -107,13 +130,12 @@ void displayTasks(const vector<Task>& taskList) {
 void loadTasksFromFile(vector<Task>& taskList) {
     ifstream file("tasks.txt");
     if (file.is_open()){
-        string description;
+        string taskName, description, dueDate;
         int priority;
-        string dueDate;
         bool completed;
 
-        while (file >> description >> priority >> dueDate >> completed){
-            Task loadedTask(description, priority, dueDate);
+        while (file >> taskName >> description >> priority >> dueDate >> completed){
+            Task loadedTask(taskName, description, priority, dueDate);
             if (completed){
                 loadedTask.markAsComplete();
             }
@@ -126,12 +148,21 @@ void loadTasksFromFile(vector<Task>& taskList) {
 }
 
 void saveTasksToFile(const vector<Task>& taskList) {
+    
+    // What I would like to add to this as a save,
+    // - description
+    // - due date
+    // - priority
+    // - completed
+
     ofstream file("tasks.txt");
 
     if (file.is_open()){
         for(const Task& task : taskList){
-            file << task.getDescription() << " " << task.isCompleted() << " "<< task.isCompleted() << "\n";            
+            file << task.getDescription() << " " << task.isCompleted() << " "<< task.isCompleted() << " " << task.getDueDate() <<  "\n";            
         }
+    }else{
+        cout << "This file is not open, and can not be saved! \n";
     }
 }
 
